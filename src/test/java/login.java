9d234a1fd7929d299c22e1bb746fae84d1ce9701
umair.hassan <umair.hassan@10pearls.local>
2019-07-18 18:26:25 +0500

    testnd work has been implemented

--- src/test/java/login.java
+++ src/test/java/login.java
@@ -1,13 +1,26 @@
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import org.testng.annotations.Test;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class login extends device_invoke {
 
-    public static void main(String[] args) throws InterruptedException, IOException {
+    @Test
+    public void loginFlow() throws InterruptedException, IOException {
 
         AndroidDriver<AndroidElement> driver= Capablities("HealthCareApp");
         System.out.println( "Device is invoked" );
 
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+}
+
+class admin{
+
+    public void adminFlow() {
+
+        adminFlow();
     }
 }
