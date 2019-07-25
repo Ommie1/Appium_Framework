0e551e8a26a7b7bbc841ec51af930bc279da6387
umair.hassan <umair.hassan@10pearls.local>
2019-07-25 19:05:01 +0500

    Basic java practice added.

--- src/Java_Basics/Java_Datatype.java
+++ src/Java_Basics/Java_Datatype.java
@@ -1,9 +1,16 @@
-public class Java_Datatype {
+//public class Java_Datatype {
+//
+//    public static void main(String[] args)
+//    {
+//
+//
+//
+//    }
+//}
 
-    public static void main(String[] args)
-    {
-        int 
-        a=b=10;
-        System.out.println(a,b);
-    }
-}
+/**
+ * Data types specify the different sizes and values that can be stored in the variable. There are two types of data types in Java:
+
+ * Primitive data types: The primitive data types include boolean, char, byte, short, int, long, float and double.
+ * Non-primitive data types: The non-primitive data types include Classes, Interfaces, and Arrays.
+ **/
