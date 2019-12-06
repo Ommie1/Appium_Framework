d27fd3e2d87ba9817dac3b8d6eafccad2c884411
Ommie1 <30703239+Ommie1@users.noreply.github.com>
2019-12-06 16:56:37 +0500

    Update Constants.java

--- src/main/java/FrameworkUtilities/JiraIntegration/Constants.java
+++ src/main/java/FrameworkUtilities/JiraIntegration/Constants.java
@@ -2,8 +2,8 @@ package FrameworkUtilities.JiraIntegration;
 
 public final class Constants {
 
-    public static final String JIRA_URL = "https://aja.atlassian.net";
-    public static final String JIRA_USERNAME = "syed.umair.sqa@gmail.com";
-    public static final String JIRA_PASSWORD = "knywADPSXRl3kIa91JfH957C";
-    public static final String JIRA_PROJECT = "MOB";
+    public static final String JIRA_URL = "";
+    public static final String JIRA_USERNAME = "";
+    public static final String JIRA_PASSWORD = "";
+    public static final String JIRA_PROJECT = "";
 }
